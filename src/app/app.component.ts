cf63a67a7c245f2f710596f7202038fda44e3805
jamesworden <jamesworden.ny@gmail.com>
2024-07-15 21:53:39 -0400

    NGRX introduction and test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { increment, decrement, reset } from './classrooms.actions';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +12,20 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  readonly #store = inject(Store<{ count: number }>);
+
+  readonly count = toSignal(this.#store.select('count'));
+
+  increment() {
+    this.#store.dispatch(increment());
+  }
+
+  decrement() {
+    this.#store.dispatch(decrement());
+  }
+
+  reset() {
+    this.#store.dispatch(reset());
+  }
+}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './classrooms.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(StoreModule.forRoot({ count: counterReducer })),
+  ],
 };
