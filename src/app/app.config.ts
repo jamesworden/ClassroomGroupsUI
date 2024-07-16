6f792787d2a8be741ce40e9e2b7b229408e46f93
jamesworden <jamesworden.ny@gmail.com>
2024-07-15 22:17:36 -0400

    Add angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { StoreModule } from '@ngrx/store';
 import { classroomsReducer } from './classrooms.reducer';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(StoreModule.forRoot({ classrooms: classroomsReducer })),
+    importProvidersFrom(StoreModule.forRoot({ classrooms: classroomsReducer })), provideAnimationsAsync(),
   ],
 };
