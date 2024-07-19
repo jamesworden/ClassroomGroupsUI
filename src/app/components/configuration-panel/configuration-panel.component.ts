7294fab19f60ef4ca742a0979389c1022e1f7db1
jamesworden <jamesworden.ny@gmail.com>
2024-07-19 12:38:43 -0400

    Things

--- src/app/components/configuration-panel/configuration-panel.component.ts
+++ src/app/components/configuration-panel/configuration-panel.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-configuration-panel',
   standalone: true,
-  imports: [],
+  imports: [
+    MatSlideToggleModule,
+    MatInputModule,
+    FormsModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './configuration-panel.component.html',
-  styleUrl: './configuration-panel.component.scss'
+  styleUrl: './configuration-panel.component.scss',
 })
 export class ConfigurationPanelComponent {
+  readonly #store = inject(Store);
 
+  averageScores = false;
+  groupingByDivision = false;
+  groupingValue = 0;
+  updatedDescription = '';
+
+  toggleGroupingType() {
+    this.groupingByDivision = !this.groupingByDivision;
+  }
+
+  updateDescription() {
+    // this.#store.dispatch(
+    //   updateClassroomDescription({
+    //     classroomId: this.viewingClassroomId(),
+    //     description: this.updatedClassroomDescription,
+    //   })
+    // );
+  }
 }
