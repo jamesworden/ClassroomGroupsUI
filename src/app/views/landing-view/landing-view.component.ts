07f3f83cb9590c1d556aca8e177d21311c046f8e
James Worden <james@jamesworden.com>
2024-12-14 16:23:32 -0500

    Landing page

--- src/app/views/landing-view/landing-view.component.ts
+++ src/app/views/landing-view/landing-view.component.ts
@@ -1,20 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component, effect, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
 import { AccountsService } from '@shared/accounts';
 import { GoogleSignInButtonComponent } from '@ui-inputs';
+import { Themes } from 'app/themes/theme.models';
+import { ThemeService } from 'app/themes/theme.service';
 
 @Component({
   selector: 'app-landing-view',
   standalone: true,
-  imports: [GoogleSignInButtonComponent],
+  imports: [GoogleSignInButtonComponent, MatIconModule, CommonModule],
   templateUrl: './landing-view.component.html',
   styleUrl: './landing-view.component.scss',
 })
 export class LandingViewComponent {
   readonly #accountsService = inject(AccountsService);
   readonly #router = inject(Router);
+  readonly #themeService = inject(ThemeService);
 
   readonly isLoggedIn = this.#accountsService.select.isLoggedIn;
+  readonly Themes = Themes;
+  readonly theme = this.#themeService.theme;
 
   constructor() {
     effect(() => {
@@ -22,5 +29,9 @@ export class LandingViewComponent {
         this.#router.navigate(['classrooms']);
       }
     });
+  }
+
+  toggleTheme() {
+    this.#themeService.toggleTheme();
   }
 }
