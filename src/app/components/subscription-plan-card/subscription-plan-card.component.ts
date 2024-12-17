0926221812a81ed6a44ce9ed479e9b393251b383
James Worden <james@jamesworden.com>
2024-12-17 07:52:11 -0500

    Update

--- src/app/components/subscription-plan-card/subscription-plan-card.component.ts
+++ src/app/components/subscription-plan-card/subscription-plan-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { SubscriptionPlan } from 'app/metadata';
 
 @Component({
   selector: 'app-subscription-plan-card',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatTooltipModule],
   templateUrl: './subscription-plan-card.component.html',
   styleUrl: './subscription-plan-card.component.scss',
 })
