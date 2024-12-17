23c1a5a1dff31343af73bd1136e931742d04bb6e
James Worden <james@jamesworden.com>
2024-12-16 21:13:43 -0500

    404

--- src/app/views/page-not-found-view/page-not-found-view.component.ts
+++ src/app/views/page-not-found-view/page-not-found-view.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ToolbarComponent } from 'app/components/toolbar/toolbar.component';
 import { Themes } from 'app/themes/theme.models';
 import { ThemeService } from 'app/themes/theme.service';
 
 @Component({
   selector: 'app-page-not-found-view',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ToolbarComponent],
   templateUrl: './page-not-found-view.component.html',
   styleUrl: './page-not-found-view.component.scss',
 })
