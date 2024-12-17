7d4bd0f1272bafcbc29e1b7aa98b433453626ed9
James Worden <james@jamesworden.com>
2024-12-16 21:06:45 -0500

    404

--- src/app/views/page-not-found-view/page-not-found-view.component.ts
+++ src/app/views/page-not-found-view/page-not-found-view.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Themes } from 'app/themes/theme.models';
+import { ThemeService } from 'app/themes/theme.service';
 
 @Component({
   selector: 'app-page-not-found-view',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './page-not-found-view.component.html',
-  styleUrl: './page-not-found-view.component.scss'
+  styleUrl: './page-not-found-view.component.scss',
 })
 export class PageNotFoundViewComponent {
+  readonly #themeService = inject(ThemeService);
 
+  readonly theme = this.#themeService.theme;
+  readonly Themes = Themes;
 }
