0d08877d6bf1128d6698198fc8252051840a8746
James Worden <james@jamesworden.com>
2024-12-16 21:28:03 -0500

    update

--- src/shared/accounts/lib/models.ts
+++ src/shared/accounts/lib/models.ts
@@ -11,8 +11,15 @@ export interface GetAccountResponse {
 export interface Subscription {
   displayName: string;
   id: string;
+  subscriptionType: SubscriptionType;
   maxClassrooms: number;
   maxConfigurationsPerClassroom: number;
   maxFieldsPerClassroom: number;
   maxStudentsPerClassroom: number;
+}
+
+export enum SubscriptionType {
+  Free = 'FREE',
+  Basic = 'BASIC',
+  Pro = 'PRO',
 }
