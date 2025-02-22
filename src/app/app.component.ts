3327b373bf68dad4b91ea954dd490e2e2700fe9f
James Worden <james@jamesworden.com>
2025-02-22 01:34:35 -0500

    Refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
-import { Component, inject } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { Component, effect, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { ThemeService } from './themes/theme.service';
 import { ClassroomsService } from '@shared/classrooms';
 import { AccountsService } from '@shared/accounts';
-import { takeUntilDestroyed, toObservable } from '@angular/core/rxjs-interop';
-import { combineLatest, filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,25 +12,17 @@ import { combineLatest, filter } from 'rxjs';
   providers: [ThemeService, ClassroomsService, AccountsService],
 })
 export class AppComponent {
-  readonly #router = inject(Router);
   readonly #accountsService = inject(AccountsService);
   readonly #classroomsService = inject(ClassroomsService);
 
-  readonly isLoggedIn$ = toObservable(this.#accountsService.select.isLoggedIn);
-  readonly accountLoading$ = toObservable(
-    this.#accountsService.select.accountLoading
-  );
+  readonly isLoggedIn = this.#accountsService.select.isLoggedIn;
+  readonly accountLoading = this.#accountsService.select.accountLoading;
 
   constructor() {
-    combineLatest([this.isLoggedIn$, this.accountLoading$])
-      .pipe(
-        takeUntilDestroyed(),
-        filter(([_, accountLoading]) => !accountLoading)
-      )
-      .subscribe(([isLoggedIn]) =>
-        isLoggedIn
-          ? this.#classroomsService.getClassroomDetails()
-          : this.#router.navigate(['/'])
-      );
+    effect(() => {
+      if (!this.accountLoading() && this.isLoggedIn()) {
+        this.#classroomsService.getClassroomDetails();
+      }
+    });
   }
 }
